61edf317783e49ecf40702106d42efb7ddbc4e3a
Alany Lourenco <alanygabi@gmail.com>
2024-08-11 01:00:43 -0300

    atualizandoflutter

--- flutter_application_1/lib/screens/login_screen.dart
+++ flutter_application_1/lib/screens/login_screen.dart
@@ -1,87 +1,140 @@
 import 'dart:ui'; // Importe o pacote dart:ui
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF00152D),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/imagens/App icon.png', // Caminho para o arquivo da imagem
-                height: 80, // Altura da imagem
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+bool _isLoading = false;
+
+Future<void> _login() async {
+  setState(() {
+    _isLoading = true;
+  });
+
+  final String username = _usernameController.text;
+  final String password = _passwordController.text;
+
+  try {
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/users/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final token = data['token'];
+
+      Navigator.pushReplacementNamed(context, '/profile');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Falha ao fazer login: ${response.body}')),
+      );
+    }
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Erro ao fazer login: $e')),
+    );
+  } finally {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: const Color(0xFF00152D),
+    body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/imagens/App icon.png',
+              height: 80,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Game Insight',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 10), // Espaçamento entre a imagem e o texto
-              const Text(
-                'Game Insight',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Usuário',
+                labelStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.person, color: Colors.white),
+                filled: true,
+                fillColor: Colors.white24,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
                 ),
               ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Usuário',
-                  labelStyle: TextStyle(color: Colors.white),
-                  prefixIcon: Icon(Icons.person, color: Colors.white),
-                  filled: true,
-                  fillColor: Colors.white24,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Senha',
+                labelStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.lock, color: Colors.white),
+                filled: true,
+                fillColor: Colors.white24,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+              ),
+              style: const TextStyle(color: Colors.white),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            _isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: _login,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF0A4892),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Entrar'),
                   ),
-                ),
-                style: TextStyle(color: Colors.white),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/register');
+              },
+              child: const Text(
+                'Não tem conta? Cadastre-se!',
+                style: TextStyle(color: Colors.blue),
               ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: Colors.white),
-                  prefixIcon: Icon(Icons.lock, color: Colors.white),
-                  filled: true,
-                  fillColor: Colors.white24,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                  ),
-                ),
-                style: TextStyle(color: Colors.white),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/profile');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF0A4892), // Cor de fundo
-                  foregroundColor: Colors.white, // Cor do texto
-                ),
-                child: const Text('Entrar'),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/register');
-                },
-                child: const Text(
-                  'Não tem conta? Cadastre-se!',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
